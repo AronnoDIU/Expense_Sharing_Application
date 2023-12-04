7d356c39394ef848e82e2e73215f6aaa8b2d87f0
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-05 04:55:39 +0600

    Expense Sharing

--- src/ExpenseSharingApp.java
+++ src/ExpenseSharingApp.java
@@ -1,4 +1,8 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -6,6 +10,7 @@ public class ExpenseSharingApp {
     private static final Scanner scanner = new Scanner(System.in);
     private static final List<User> users = new ArrayList<>();
     private static final List<Expense> expenses = new ArrayList<>();
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Expense Sharing App");
@@ -107,4 +112,157 @@ public class ExpenseSharingApp {
     }
 
     // Add more methods for expense-related features, exporting, filtering, splitting, editing, and graphing
+
+    private static void addExpense(User currentUser) {
+        System.out.print("\nEnter the name of the person incurring the expense: ");
+        String person = scanner.nextLine();
+
+        if (!people.contains(person)) {
+            System.out.println("Invalid person. Please enter a valid name.");
+            return;
+        }
+
+        System.out.print("Enter the expense amount for " + person + ": ");
+        double amount = scanner.nextDouble();
+        scanner.nextLine(); // Consume the newline character
+
+        System.out.print("Enter the expense category: ");
+        String category = scanner.nextLine();
+
+        System.out.print("Enter the date of the expense (YYYY-MM-DD): ");
+        String dateString = scanner.nextLine();
+        Date date = parseDate(dateString);
+
+        System.out.print("Enter a comment for the expense: ");
+        String comment = scanner.nextLine();
+
+        expenses.add(new Expense(person, amount, category, date, comment));
+        System.out.println("Expense added successfully!");
+    }
+
+    private static void exportToCSV() {
+        System.out.print("Enter the file name to export to (e.g., expenses.csv): ");
+        String fileName = scanner.nextLine();
+
+        try (FileWriter csvWriter = new FileWriter(fileName)) {
+            csvWriter.append("Person,Amount,Category,Date,Comment\n");
+
+            for (Expense expense : expenses) {
+                csvWriter.append(expense.person)
+                        .append(",")
+                        .append(String.valueOf(expense.amount))
+                        .append(",")
+                        .append(expense.category)
+                        .append(",")
+                        .append(dateFormat.format(expense.date))
+                        .append(",")
+                        .append(expense.comment)
+                        .append("\n");
+            }
+
+            System.out.println("Export to CSV successful!");
+        } catch (IOException e) {
+            System.out.println("Error exporting to CSV: " + e.getMessage());
+        }
+    }
+
+    private static void filterExpenses() {
+        System.out.print("\nEnter the person's name to filter expenses: ");
+        String person = scanner.nextLine();
+
+        if (!people.contains(person)) {
+            System.out.println("Invalid person. Please enter a valid name.");
+            return;
+        }
+
+        System.out.println("\nFiltered Expenses for " + person + ":");
+
+        for (Expense expense : expenses) {
+            if (expense.person.equals(person)) {
+                System.out.println("Amount: " + expense.amount +
+                        ", Category: " + expense.category +
+                        ", Date: " + dateFormat.format(expense.date) +
+                        ", Comment: " + expense.comment);
+            }
+        }
+    }
+
+    private static void equallySplitExpenses() {
+        System.out.print("\nEnter the common expense amount to be equally split: ");
+        double commonExpense = scanner.nextDouble();
+        scanner.nextLine(); // Consume the newline character
+
+        int numPeople = people.size();
+
+        for (String person : people) {
+            double equalShare = commonExpense / numPeople;
+            expenses.add(new Expense(person, equalShare, "Equal Split", new Date(), "Equally split expense"));
+        }
+
+        System.out.println("Expenses equally split among participants!");
+    }
+
+    private static void editOrDeleteExpense(User currentUser) {
+        System.out.print("\nEnter the index of the expense to edit or delete: ");
+        int index = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        if (index >= 0 && index < expenses.size()) {
+            Expense expense = expenses.get(index);
+
+            if (expense.person.equals(currentUser.username)) {
+                System.out.print("Do you want to edit (E) or delete (D) the expense? ");
+                char choice = scanner.next().charAt(0);
+                scanner.nextLine(); // Consume the newline character
+
+                switch (choice) {
+                    case 'E':
+                    case 'e':
+                        editExpense(expense);
+                        break;
+                    case 'D':
+                    case 'd':
+                        expenses.remove(index);
+                        System.out.println("Expense deleted successfully!");
+                        break;
+                    default:
+                        System.out.println("Invalid choice. No changes made.");
+                        break;
+                }
+            } else {
+                System.out.println("You can only edit or delete your own expenses.");
+            }
+        } else {
+            System.out.println("Invalid index. No changes made.");
+        }
+    }
+
+    private static void editExpense(Expense expense) {
+        System.out.print("Enter the new expense amount: ");
+        double newAmount = scanner.nextDouble();
+        scanner.nextLine(); // Consume the newline character
+
+        System.out.print("Enter the new expense category: ");
+        String newCategory = scanner.nextLine();
+
+        System.out.print("Enter the new date of the expense (YYYY-MM-DD): ");
+        String newDateString = scanner.nextLine();
+        Date newDate = parseDate(newDateString);
+
+        System.out.print("Enter a new comment for the expense: ");
+        String newComment = scanner.nextLine();
+
+        expense.amount = newAmount;
+        expense.category = newCategory;
+        expense.date = newDate;
+        expense.comment = newComment;
+
+        System.out.println("Expense edited successfully!");
+    }
+
+    private static void expenseSummaryGraphs() {
+        // Add code for generating graphs (e.g., pie charts, bar graphs) based on the expenses
+        // You may use external libraries like JFreeChart for graphical representations
+        System.out.println("Graphical representation feature coming soon!");
+    }
 }
